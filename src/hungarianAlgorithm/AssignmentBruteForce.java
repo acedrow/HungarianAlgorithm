934c0e43e54ebd765dbfd7e02a9d096b0dbb5d8d
Linden Holt <ace.holt@gmail.com>
2018-06-02 16:31:12 -0400

    Brute force testing completed

--- src/hungarianAlgorithm/AssignmentBruteForce.java
+++ src/hungarianAlgorithm/AssignmentBruteForce.java
@@ -1,8 +1,6 @@
 package hungarianAlgorithm;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class AssignmentBruteForce {
 	
@@ -12,8 +10,10 @@ public class AssignmentBruteForce {
 	
 	//main method for testing purposes
 	public static void main (String[] args){
-		int[][] arr1 = { { 56, 23, 89, 1, 3 }, { 7, 14, 75, 90, 22 }, { 9, 47, 50, 12, 84 },
-                { 32, 51, 94, 29, 16 }, { 6, 7, 30, 85, 99 } };
+		 int[][] arr1 = { { 56, 23, 89, 1, 3 }, { 7, 14, 75, 90, 22 }, { 9, 47, 50, 12, 84 },
+                { 32, 51, 94, 29, 16 }, { 6, 7, 30, 85, 99 } }; 
+		
+		int[][] arr11 = {{1,2,3},{6,4,1},{0,9,3}};
 		
 		bruteForceSolution(arr1);
 		System.out.println(min);
@@ -24,41 +24,53 @@ public class AssignmentBruteForce {
 	public static int bruteForceSolution(int[][] arr1){
 		n = arr1.length;
 		ArrayList<Integer> usedCol = new ArrayList<Integer>();
-		usedCol.add(-1);
 		rowIterator (0, 0, usedCol, arr1);
 		
 		return 0;
 	}
 	
 	public static int rowIterator(int currRow, int runningSum, ArrayList<Integer> usedCol, int[][] arr1){
-		if (currRow >= n) //our end condition. Each recursive call increments the currRow, 
+		ArrayList<Integer> myUsedCol = new ArrayList<Integer>(); //copy previously used column values into iteration-specific instance
+		for (int col : usedCol){
+			myUsedCol.add(col);
+		}
+		if (currRow >= n){ //our end condition. Each recursive call increments the currRow, 
 			//when we get past the bottom row (currRow == n), we've summed one complete 
 			if (runningSum < min){
 				min = runningSum;
 				solution.clear();
-				for (int i : usedCol){
+				for (int i : myUsedCol){
 					solution.add(i);
-				}
+				}	
 			}
-		
+		return 1;
+		}
 		for (int col = 0; col < n; col++){ 
-			boolean used = true;
+			while ((myUsedCol.size()) > currRow){ //make sure our previously used column list is the correct length
+				myUsedCol.remove((myUsedCol.size()-1));
+			}
+			boolean used = false;
 			int i = 0;
 			//need to add code to remove the second-most-recent usedCol entry once the recursive call is made.
 			do {
-				if (usedCol.size() == 0 || usedCol.get(i) == col){
+				
+				if (myUsedCol.size() == 0){
 					used = false;
+					break;
 				}
+				if (myUsedCol.get(i) == col){
+					used = true;
+				}
+				
 				i++;
 			}
-			while (i <= usedCol.size() && used);
+			while (i < myUsedCol.size());
 				
-				
-			
 				if (!used){
-					runningSum += arr1[currRow][col]; //add the value to running sum
-					usedCol.add(col); //add the column to our used columns list
-					rowIterator(currRow++, runningSum, usedCol, arr1);
+					int myRunningSum = runningSum + arr1[currRow][col]; //add the value to running sum
+					myUsedCol.add(col); //add the column to our used columns list
+					int nextRow = currRow+1;
+					rowIterator(nextRow, myRunningSum, myUsedCol, arr1);
 				}
 		}
 		return 0;
